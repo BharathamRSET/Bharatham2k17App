8ebb8553e88827efbd38e6ac2d008dd9cedd4939
Joseph Varghese <josephvarghese.rms@gmail.com>
2017-08-14 20:01:27 +0530

    Selfie Gallery...adapter etc added

--- app/src/main/java/com/roundmelon/jv/bharatham2k17/MyAdapter.java
+++ app/src/main/java/com/roundmelon/jv/bharatham2k17/MyAdapter.java
@@ -1,101 +1,119 @@
 package com.roundmelon.jv.bharatham2k17;
 
-/**
- * Created by Joseph on 14/08/17.
- */
-
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.List;
 
+/**
+ * Created by Joseph on 16/03/17.
+ */
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private List<String> values;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView event_name;
-        public TextView description;
-        public TextView first_prize;
-        public TextView second_prize;
-        public TextView third_prize;
-        public TextView first_prize_group;
-        public TextView third_prize_group;
-        public TextView second_prize_group;
+    private Context context;
+    private List<Upload> uploads;
 
-        public View layout;
+    public MyAdapter(Context context, List<Upload> uploads) {
+        this.uploads = uploads;
+        this.context = context;
+    }
 
-        public ViewHolder(View v) {
-            super(v);
-            layout = v;
-            event_name = (TextView) v.findViewById(R.id.event_name);
-            description = (TextView) v.findViewById(R.id.description);
-            first_prize = (TextView) v.findViewById(R.id.first_prize);
-            second_prize = (TextView) v.findViewById(R.id.second_prize);
-            third_prize = (TextView) v.findViewById(R.id.third_prize);
-            first_prize_group = (TextView) v.findViewById(R.id.first_prize_group);
-            second_prize_group = (TextView) v.findViewById(R.id.s_prize_group);
-            third_prize_group = (TextView) v.findViewById(R.id.second_prize_group);
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.gallery_thumbnail, parent, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Upload upload = uploads.get(position);
+
+        //holder.textViewName.setText(upload.getName());
+
+        Glide.with(context).load(upload.getUrl())
+                .thumbnail(0.5f)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(holder.imageGallery);
+    }
+
+    @Override
+    public int getItemCount() {
+        return uploads.size();
+    }
+
+    public interface ClickListener {
+        void onClick(View view, int position);
+
+        void onLongClick(View view, int position);
+    }
+
+    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+        private GestureDetector gestureDetector;
+        private MyAdapter.ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final MyAdapter.ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    }
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildPosition(child));
+            }
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
         }
     }
 
-    public void add(int position, String item) {
-        values.add(position, item);
-        notifyItemInserted(position);
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textViewName;
+        public ImageView imageGallery;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            //textViewName = (TextView) itemView.findViewById(R.id.textViewName);
+            imageGallery = (ImageView) itemView.findViewById(R.id.thumbnail);
+        }
     }
-
-    public void remove(int position) {
-        values.remove(position);
-        notifyItemRemoved(position);
-    }
-
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public MyAdapter(List<String> myDataset) {
-        values = myDataset;
-    }
-
-    // Create new views (invoked by the layout manager)
-    @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
-        // create a new view
-        LayoutInflater inflater = LayoutInflater.from(
-                parent.getContext());
-        View v =
-                inflater.inflate(R.layout.row_layout, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
-    }
-
-    // Replace the contents of a view (invoked by the layout manager)
-    @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        final String name = values.get(position);
-        holder.event_name.setText(name);
-        holder.event_name.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                remove(position);
-            }
-        });
-
-        holder.event_name.setText("Footer: " + name);
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return values.size();
-    }
-
 }
