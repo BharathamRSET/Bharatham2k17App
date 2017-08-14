ac405403c3b82be8f58cb5dbd4cbe235ac528d06
Joseph Varghese <josephvarghese.rms@gmail.com>
2017-08-14 17:54:29 +0530

    HomeActivity and splash

--- app/src/main/java/com/roundmelon/jv/bharatham2k17/Splash.java
+++ app/src/main/java/com/roundmelon/jv/bharatham2k17/Splash.java
@@ -1,7 +1,12 @@
 package com.roundmelon.jv.bharatham2k17;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +14,44 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+
+        final Animation an1= AnimationUtils.loadAnimation(getBaseContext(),R.anim.idle);
+        final Animation an2= AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
+        final RelativeLayout rl=(RelativeLayout) findViewById(R.id.rl);
+        rl.startAnimation(an1);
+        an1.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+
+
+                //rl.startAnimation(an2);
+                finish();
+
+
+
+
+                Intent intent = new Intent(Splash.this,HomeActivity.class);
+                startActivity(intent);
+
+
+            }
+
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+
     }
 }
